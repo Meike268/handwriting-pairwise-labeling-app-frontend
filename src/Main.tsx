31e3d76e160337e5fd3cf20383fb280b4daa3f42
Lukas <lukas.pieger@prontoweb.de>
2024-07-27 15:19:32 +0200

    feat: Basic styling of TaskPage

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,13 +1,12 @@
 import React, {useContext} from "react";
-import Feature from "./util/Feature";
-import HintPage from "./pages/HintPage";
 import {DisplayContext} from "./util/DisplayContext";
 import {post, put} from "./authentication/io";
 import {APP_BATCH_LABELING_SAMPLE, APP_BATCH_LABELING_PATH, BACKEND_ANSWER} from "./constants/Urls";
 import TaskPage from "./pages/TaskPage";
 import { useNavigate, useParams} from "react-router-dom";
 import {BatchContext} from "./util/BatchProvider";
-import {Score, TaskBatch} from "./entities/Batch";
+import {Sample, Score, TaskBatch} from "./entities/Batch";
+import ProgressBar from "./components/ProgressBar";
 
 const NAVIGATION_BUTTON_RELATIVE_WIDTH = 15
 
@@ -19,19 +18,17 @@ const Main: React.FC = () => {
     const [maybeBatch, setBatch] = useContext(BatchContext)!
     const batch: TaskBatch = maybeBatch!
 
-    if (sampleInd === 0)
-        return <HintPage feature={Feature.BASELINE} onStart={() => nextPage()}/> // TODO make this an actual question hint
+    const currentSample = sampleInd === 0 ? null : batch.samples[sampleInd-1]
 
-    const currentSample = batch.samples[sampleInd-1]
+    async function updateScore(score: Score, sample: Sample) {
 
-    async function updateScore(score: Score) {
-        const exists = currentSample.score !== undefined
+        const exists = sample.score !== undefined
 
-        currentSample.score = score
+        sample.score = score
         setBatch(batch)
 
         const answer = {
-            sampleId: currentSample.id,
+            sampleId: sample.id,
             questionId: batch.question.id,
             score: score
         }
@@ -50,8 +47,9 @@ const Main: React.FC = () => {
         }
     }
 
-    function onSubmit(score: Score) {
-        updateScore(score).then()
+    function onSubmit(score: Score | null) {
+        if (score !== null)
+            updateScore(score, currentSample!).then()
         nextPage()
     }
 
@@ -75,11 +73,11 @@ const Main: React.FC = () => {
                 <button onClick={() => prevPage()} style={{width: NAVIGATION_BUTTON_RELATIVE_WIDTH + "%", height: "100%", color: "lightgreen", fontWeight: "bolder", cursor: "pointer"}}>zurück</button>
             }
             <h1 style={{height: "min-content", maxWidth: (100-NAVIGATION_BUTTON_RELATIVE_WIDTH*2) + "%"}}>{batch.question.description}</h1>
-            { currentSample.score === undefined ? <div style={{width: NAVIGATION_BUTTON_RELATIVE_WIDTH + "%"}}/> :
+            { currentSample?.score === undefined ? <div style={{width: NAVIGATION_BUTTON_RELATIVE_WIDTH + "%"}}/> :
                 <button onClick={() => nextPage()} style={{width: NAVIGATION_BUTTON_RELATIVE_WIDTH + "%", height: "100%", color: "lightgreen", fontWeight: "bolder", cursor: "pointer"}}>weiter</button>
             }
         </div>
-        {/*<ProgressBar style={{height: "1.5%"}} current={page.number} end={8 * (1+wordIds.length)}/>*/}
+        <ProgressBar style={{height: "1.5%"}} current={sampleInd} end={batch.samples.length+1}/>
         <div style={{height: "92.5%", width: "100%", display:"flex", justifyItems: "center", alignItems: "center"}}>
             <TaskPage question={batch.question} referenceSentence={batch.referenceSentence} examplePair={batch.examplePair} sample={currentSample} onSubmit={(score) => onSubmit(score)}/>
         </div>
--- src/pages/TaskPage.tsx
+++ src/pages/TaskPage.tsx
@@ -1,24 +1,62 @@
 import "./QuestionPage.css"
 import React from "react";
 import {ExamplePair, Question, ReferenceSentence, Sample, Score} from "../entities/Batch";
+import QuestionDescription from "../components/QuestionDescription";
+import ScoreDescriptor from "../components/ScoreDescriptor";
 
-const TaskPage: React.FC<{question: Question, referenceSentence: ReferenceSentence, examplePair: ExamplePair, sample: Sample, onSubmit: (score: Score)=>void}> = ({question, referenceSentence, examplePair, sample,  onSubmit=()=>{}}) => {
-    console.log(sample.id)
-    return <div style={{height: "100%", width: "100%"}}>
-        Question: {JSON.stringify(question)}<br/>
-        Sentence: {JSON.stringify(referenceSentence)}<br/>
-        <img src={sample.image.src} alt={"sample"}/>
-        <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
-            <img src={examplePair.positive.src} alt={"positive_example"}/>
-            <button onClick={() => onSubmit(0)}>0</button>
-            <button onClick={() => onSubmit(1)}>1</button>
-            <button onClick={() => onSubmit(2)}>2</button>
-            <button onClick={() => onSubmit(3)}>3</button>
-            <button onClick={() => onSubmit(4)}>4</button>
-            <img src={examplePair.negative.src} alt={"negative_example"}/>
+const TaskPage: React.FC<{
+    question: Question,
+    referenceSentence: ReferenceSentence,
+    examplePair: ExamplePair,
+    sample: Sample | null,
+    onSubmit: (score: Score | null) => void
+}> = ({
+          question, referenceSentence, examplePair, sample, onSubmit = () => {
+    }
+      }) => {
+    return <div style={{
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between"
+    }}>
+        <div style={{marginTop: "5%"}}>{<QuestionDescription question={question}/>}</div>
+        {sample && <img src={sample.image.src} alt={"sample"}/>}
+        <div style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            width: "100%",
+            height: "15%"
+        }}>
+            <img src={examplePair.positive.src} alt={"positive_example"} style={{width: "35%", objectFit: "contain"}}/>
+            {sample ? <div style={{
+                width: "30%",
+                display: "flex",
+                justifyContent: "space-around"
+            }}>
+                {[...Array(5)].map((_, score) => {
+                    return <button
+                        className={"score-button"}
+                        key={`${sample.id}_${score}`}  // extra key per sample to remove hasactive-css-class between samples
+                        onClick={() => onSubmit(score as Score)}
+                        style={{
+                            width: "19%",
+                            backgroundColor: sample.score === score ? "green" : undefined,
+                            display: "flex",
+                            flexDirection: "column",
+                            justifyContent: "flex-start",
+                            alignItems: "center"
+                        }}>
+                        <div style={{color: "lightgreen"}}><b>{score}</b></div>
+                        <ScoreDescriptor score={score} question={question}/>
+                    </button>
+                })}
+            </div> : <button style={{width: "100%", margin: "10px"}} onClick={() => onSubmit(null)}>Start</button>}
+            <img src={examplePair.negative.src} alt={"negative_example"}
+                 style={{width: "35%", objectFit: "contain"}}/>
         </div>
-
-
     </div>
 }
 
