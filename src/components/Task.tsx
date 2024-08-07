4e4657541608d26ff4274faadf7e1d1944224117
Lukas <lukas.pieger@prontoweb.de>
2024-08-07 21:02:06 +0200

    style: Make answer-buttons bigger

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -23,50 +23,47 @@ const Task: React.FC<{
     }}>
 
         <div style={{
-            width: "100%",
             display: "flex",
             flexDirection: "row",
-            justifyContent: "center"
+            justifyContent: "center",
         }}>
-            <div style={{marginTop: "5%"}}>{<QuestionDescription question={question}/>}</div>
+            <div style={{marginTop: "5%", margin: "10px"}}>{<QuestionDescription question={question}/>}</div>
         </div>
-
-        {sample && <Image src={sample.image} alt={"sample"}/>}
-
         <div style={{
             display: "flex",
             flexDirection: "row",
             justifyContent: "center",
-            width: "100%",
-            height: "20%",
-            marginBottom: "5%"
         }}>
-            {sample ? <div style={{
-                maxWidth: "70%",
-                display: "flex",
-                flexDirection: "column",
-                gap: "10px"
-            }}>
-                {[...Array(5)].map((_, scoreButtonIndex) => {
-                    let score = scoreButtonIndex + 1
-                    return <button
-                        className={"score-button"}
-                        key={`${sample.id}_${score}`}  // extra key per sample to remove hasactive-css-class between samples
-                        onClick={() => onSubmit(score as Score)}
-                        style={{
-                            width: "100%",
-                            backgroundColor: sample.score === score ? "green" : undefined,
-                            display: "flex",
-                            flexDirection: "row",
-                            justifyContent: "flex-start",
-                            alignItems: "center",
-                            padding: "10px"
-                        }}>
-                        <div style={{color: "lightgreen"}}><b>{score}</b></div>
-                        <ScoreDescriptor score={score} question={question}/>
-                    </button>
-                })}
-            </div> : <button style={{width: "30%", margin: "10px"}} onClick={() => onSubmit(null)}>Start</button>}
+            {sample && <Image style={{margin: "10px", maxWidth: "700px"}} src={sample.image} alt={"sample"}/>}
+        </div>
+        <div className={"AnswerOrStartWrapper"} style={{
+            height: "30%"
+        }}>
+            {sample === null
+                ?
+                <button className={"StartButton"} style={{height: "100%", width: "30%", textAlign: "center"}}
+                                            onClick={() => onSubmit(null)}>Start</button>
+                :
+                <div className={"AnswerWrapper"} style={{height: "100%", width: "30%", display: "flex", flexDirection: "column", justifyContent: "space-between", margin: "auto"}}>
+                    {[...Array(5)].map((_, score) => {
+                        return <button
+                            className={"score-button"}
+                            key={`${sample.id}_${score}`}  // extra key per sample to remove hasactive-css-class between samples
+                            onClick={() => onSubmit(score as Score)}
+                            style={{
+                                height: 98/5+"%",
+                                backgroundColor: sample.score === score ? "green" : undefined,
+                                display: "flex",
+                                flexDirection: "row",
+                                justifyContent: "left",
+                                alignItems: "center",
+                                padding: "5px",
+                            }}>
+                            <div style={{color: "lightgreen", marginRight: "10px"}}><b>{score+1}</b></div>
+                            <ScoreDescriptor score={score+1} question={question}/>
+                        </button>
+                    })}
+                </div>}
         </div>
     </div>
 }
